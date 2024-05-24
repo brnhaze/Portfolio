706da1674cff50548e4bfaf0cf028342928d91c3
Ashwin <ashwin_pal@hotmail.com>
2024-05-24 14:44:29 -0700

    changes

---
+++ Data/Cleaned/SQL/2 Upload FIle.sql
@@ -0,0 +1,8 @@
+SELECT * FROM movies
+
+-- GUI was used to upload the file
+-- Issues
+	-- Delimeter issues with '""' or "''"
+
+SELECT * FROM movies
+
---
+++ Data/Cleaned/SQL/3 Quality Assurance.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM information_schema.tables
+WHERE table_schema = 'public'
+
+SELECT * FROM movies
