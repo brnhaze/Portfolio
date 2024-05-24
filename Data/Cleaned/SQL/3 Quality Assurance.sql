c0193208126cb59fb2ca8d7835e8c8598d6ed913
Ashwin <ashwin_pal@hotmail.com>
2024-05-24 15:40:15 -0700

    update

--- Data/Cleaned/SQL/3 Quality Assurance.sql
+++ Data/Cleaned/SQL/3 Quality Assurance.sql
@@ -1,5 +1,33 @@
+-- Retrieve & List Tables
 SELECT *
 FROM information_schema.tables
 WHERE table_schema = 'public'
 
 SELECT * FROM movies
+
+-- Remove the characters [' and '] -- Failed
+	-- Attempt 1:
+		UPDATE movies
+		SET
+			title = REGEXP_REPLACE(title, '^\\[\\'' , ''),
+			title = REGEXP_REPLACE(title, '\\'\\]$', ''),
+			description = REGEXP_REPLACE(description, '^\\[\\'' , ''),
+			description = REGEXP_REPLACE(description, '\\'\\]$', ''),
+			genre = REGEXP_REPLACE(genre, '^\\[\\'' , ''),
+			genre = REGEXP_REPLACE(genre, '\\'\\]$', '');
+
+	-- Attempt 2:
+		UPDATE movies -- 
+		SET
+			directed_by = REGEXP_REPLACE(directed_by, '^\\[\\'' , ''),
+			directed_by = REGEXP_REPLACE(directed_by, '\\'\\]$', '');
+
+	-- Attempt 3:
+		UPDATE movies
+		SET
+			directed_by = REGEXP_REPLACE(directed_by, '^\\\\[\\\\'' , ''),
+			directed_by = REGEXP_REPLACE(directed_by, '\\\\'\\]$', '');
+			
+	-- Resoluton: Imported Excel "Clean" .csv file
+		-- resolution shows benefits of learning more than one platform 
+
